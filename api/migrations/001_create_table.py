1a114ac5ef6adf62a31881adcfc5e3e5172f5cd2
Brad <bradcochi@gmail.com>
2023-09-29 14:15:22 -0600

    started creating backend endpoints and CRUD

--- api/migrations/001_create_table.py
+++ api/migrations/001_create_table.py
@@ -19,17 +19,18 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE users (
+        CREATE TABLE accounts (
             id SERIAL PRIMARY KEY NOT NULL,
             first_name VARCHAR(1000) NOT NULL,
             last_name VARCHAR(1000) NOT NULL,
             email VARCHAR(1000) NOT NULL,
-            username VARCHAR(1000) NOT NULL
+            username VARCHAR(1000) NOT NULL,
+            password VARCHAR(1000) NOT NULL
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE users;
+        DROP TABLE accounts;
         """,
     ],
 ]
