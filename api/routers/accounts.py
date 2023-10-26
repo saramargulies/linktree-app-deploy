b8dfc2eed1cba0c1b3c89e14d40646d7941655fb
Sara <saraaustenm@gmail.com>
2023-10-26 16:40:37 -0400

    finished accounts

--- api/routers/accounts.py
+++ api/routers/accounts.py
@@ -36,7 +36,7 @@ async def get_protected(
 async def get_token(
     request: Request,
     account: AccountOut = Depends(authenticator.try_get_current_account_data),
-) -> AccountToken | None:
+) -> Union[AccountToken, None]:
     if account and authenticator.cookie_name in request.cookies:
         return {
             "access_token": request.cookies[authenticator.cookie_name],
