6dcdd26f07a705c8a9c2b1ff13d55ffa78a94537
Sara <saraaustenm@gmail.com>
2023-10-26 01:54:46 -0400

    main.py pathway typo

--- api/tests/test_links.py
+++ api/tests/test_links.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from main import app
+from ..main import app
 from queries.links import LinkRepository
 from models import LinkIn
 from authenticator import authenticator
