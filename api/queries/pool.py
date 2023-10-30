0a31b7e4ede1cc8525736ab33fb17a24c681ad39
Sara <saraaustenm@gmail.com>
2023-10-30 18:20:42 -0400

    difference between environ and .get

--- api/queries/pool.py
+++ api/queries/pool.py
@@ -3,4 +3,5 @@ from psycopg_pool import ConnectionPool
 
 
 POSTGRES_URL = os.environ.get("POSTGRES_URL")
+print("===================================","THE POSTGRES URL (.GET) IS=", POSTGRES_URL, "THE OS.ENVIRON METHOD =", os.environ["POSTGRES_URL"])
 pool = ConnectionPool(conninfo=POSTGRES_URL)
