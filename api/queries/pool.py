44e49d64725e3b9bead4c2ef21cf09f7a8663843
Sara <saraaustenm@gmail.com>
2023-10-30 18:30:28 -0400

    no =

--- api/queries/pool.py
+++ api/queries/pool.py
@@ -2,5 +2,5 @@ import os
 from psycopg_pool import ConnectionPool
 
 
-POSTGRES_URL = os.environ.get("DATABASE_URL") + "="
+POSTGRES_URL = os.environ.get("DATABASE_URL")
 pool = ConnectionPool(conninfo=POSTGRES_URL)
