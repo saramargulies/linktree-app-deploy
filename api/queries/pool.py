22c3b3b010aef4fd787cb41914b66cf6a25fd92d
Sara <saraaustenm@gmail.com>
2023-10-30 18:27:30 -0400

    back to database_url

--- api/queries/pool.py
+++ api/queries/pool.py
@@ -2,6 +2,5 @@ import os
 from psycopg_pool import ConnectionPool
 
 
-POSTGRES_URL = os.environ.get("POSTGRES_URL")
-print("===================================","THE POSTGRES URL (.GET) IS=", POSTGRES_URL, "THE OS.ENVIRON METHOD =", os.environ["POSTGRES_URL"])
+POSTGRES_URL = os.environ.get("DATABASE_URL") + "="
 pool = ConnectionPool(conninfo=POSTGRES_URL)
