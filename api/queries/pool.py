0218519f2c47b4f00a45652381016bc265d626ec
Sara <saraaustenm@gmail.com>
2023-10-30 17:49:59 -0400

    db set up

--- api/queries/pool.py
+++ api/queries/pool.py
@@ -3,4 +3,4 @@ from psycopg_pool import ConnectionPool
 
 
 POSTGRES_URL = os.environ.get("POSTGRES_URL")
-pool = ConnectionPool(conninfo=os.environ.get(POSTGRES_URL))
+pool = ConnectionPool(conninfo=POSTGRES_URL)
