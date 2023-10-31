f1e66e059edced1e362c31d9d5c1d034fd548ba2
Sara <saraaustenm@gmail.com>
2023-10-31 00:33:58 -0400

    env console

--- ghi/src/Global/SignUp.jsx
+++ ghi/src/Global/SignUp.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import AlertError from "./AlertError";
 
 const SignUp = () => {
-  console.log(process.env.FASTAPI_URL)
+  console.log(process.env)
   const navigate = useNavigate();
   const [signup, signupResult] = useSignupMutation();
   const [errorMessage, setErrorMessage] = useState("");
